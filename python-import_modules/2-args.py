80804c775b8a5ea2820b9255d6a7e3996363fdcf
KevinYeff <kevinyeff94@gmail.com>
2023-01-18 19:02:19 -0500

    fix: Now displays the correct string when passed a single argument.

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,6 +4,8 @@ if __name__ == "__main__":
     arglen = len(sys.argv) - 1
     if arglen == 0:
         print("{} arguments.".format(arglen))
+    elif arglen == 1:
+        print("{} argument:".format(arglen))
     else:
         print("{} arguments:".format(arglen))
     for i in range(1, arglen + 1):
