4829a649fa33a64d47c57c2d2473ad00c5b4d247
KevinYeff <kevinyeff94@gmail.com>
2023-01-24 17:48:37 -0500

    fix: now prints the correct values for the case idx = 0.

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,7 +2,10 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0:
         return my_list
-    for i in my_list[0:]:
-        if i == idx:
-            del my_list[i]
+    elif idx >= len(my_list):
+        return my_list
+    else:
+        for i in range(len(my_list)):
+            if i == idx:
+                del my_list[idx]
     return my_list
