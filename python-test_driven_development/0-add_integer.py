6fe94327fa069b603ced75b3b9e530ff687cbd99
KevinYeff <kevinyeff94@gmail.com>
2023-02-05 11:51:41 -0500

    test: testing checker

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This is "0-add_integer" module
+this module contains add_integers function that adds two integers
+"""
+
+
+def add_integer(a, b=98):
+    """Return the addition of two numbers with spcific validation"""
+    return a + b
