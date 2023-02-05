6697b17e8c3d7ad4870ae7a39df821afaebde020
KevinYeff <kevinyeff94@gmail.com>
2023-02-05 16:05:11 -0500

    test: validating on return.

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -12,11 +12,7 @@ def add_integer(a, b=98):
     b must be an integer or float.
     """
     if type(a) is not int and type(a) is not float:
-        raise TypeError("a must be and integer")
-    elif type(b) is not int and type(b) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    elif type(a) is float:
-        a = int(a)
-    elif type(b) is float:
-        b = int(b)
-    return a + b
+    return int(a) + int(b)
