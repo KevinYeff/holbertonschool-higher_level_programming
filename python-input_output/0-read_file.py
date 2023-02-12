f891fc5f4270c1e28031a461ffb05125e1c86f28
KevinYeff <kevinyeff94@gmail.com>
2023-02-12 08:58:42 -0500

    feat: program that reads a text file and pritns it to the stdout

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 0, Read file
+"""
+
+
+def read_file(filename=""):
+    """
+    Open and print a file
+    """
+    with open("my_file_0.txt", encoding="utf-8") as fd:
+        print(fd.read(), end="")
