a34c90c84879e8087f21bd85621245c580c5bd09
KevinYeff <kevinyeff94@gmail.com>
2023-02-19 21:12:32 -0500

    fix: now returns an empty list

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -55,6 +55,6 @@ class Base:
         Method that returns  a list with the json string representation
         """
         if json_string is None or len(json_string) == 0:
-            return "[]"
+            json_string = "[]"
         else:
             return json.loads(json_string)
