22270d61596e7195e04849e8297ab4297a73f7e1
KevinYeff <kevinyeff94@gmail.com>
2023-03-22 09:54:08 -0500

    feat: test safe of sql injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+takes an argument as a value and displays the value that matchesargument
+in the table from the database
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    # Connecting to database
+    database = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                               passwd=argv[2], database=argv[3])
+    # Creating a cursor to execute queries
+    cursor = database.cursor()
+    # value that matches the argument but safely
+    cursor.execute(
+        "SELECT * FROM states WHERE BINARY name = %s", [argv[4]])
+    # Retrieving the results of the consult
+    for row in cursor.fetchall():
+        if row[1] == argv[4]:
+            print(row)
+    # Closing
+    cursor.close()
+    database.close()
