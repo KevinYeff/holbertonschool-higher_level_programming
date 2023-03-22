284a8af68a5ca4244b9ff81b84ad80eb0f3470fa
KevinYeff <kevinyeff94@gmail.com>
2023-03-22 09:19:48 -0500

    feat: List the value that matches the argument

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+takes an argument as a value and displays the value that matchesargument
+in the table from the database
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    # Connecting to database
+    database = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                               passwd=argv[2], database=argv[3])
+    # Creating a cursor to execute queries
+    cursor = database.cursor()
+    # value that matches the argument
+    cursor.execute(
+        "SELECT * FROM states WHERE name=%s", [argv[4]])
+    # Retrieving the results of the consult
+    for row in cursor.fetchall():
+        print(row)
+    # Closing
+    cursor.close()
+    database.close()
