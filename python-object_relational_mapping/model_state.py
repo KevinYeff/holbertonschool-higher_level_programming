6613bfbb71c8edfe9225c5c09b0ff8fa296c719e
KevinYeff <kevinyeff94@gmail.com>
2023-03-23 06:45:55 -0500

    feat: Python file that contains the class definition of a State and inherits from Base declarative

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+"""
+Defines the class state that inherits from Base
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ State class; links to MySQL table satates"""
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
