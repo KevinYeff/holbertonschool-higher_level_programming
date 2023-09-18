7ceff7ea934196387e9ad2aeffa8956e6637d22d
KevinYeff <kevinyeff94@gmail.com>
2023-09-17 23:50:19 -0500

    feat: script that executes a javascript even if is imported in the head tag

--- javascript-dom_manipulation/8-script.js
+++ javascript-dom_manipulation/8-script.js
@@ -1,13 +1,20 @@
-// getting the element with id hello
-const hello_id = document.getElementById('hello')
-//URL
-apiURL = 'https://hellosalut.stefanbohacek.dev/?lang=fr'
+// https://stackoverflow.com/questions/5704924/
+window.onload = function() {
+    // console.log("DOM fully loaded");
+    // getting the element with id hello
+    const hello_id = document.getElementById('hello')
+    //URL
+    apiURL = 'https://hellosalut.stefanbohacek.dev/?lang=fr'
 
-fetch(apiURL)
-    .then(function(importing_hello){
-        return importing_hello.json();
-    })
-    .then(function(hello_to_display){
-        const hello_word = hello_to_display.hello;
-        hello_id.textContent = hello_word;
-    });
+    fetch(apiURL)
+        .then(function(importing_hello){
+            //get the json
+            return importing_hello.json();
+        })
+        .then(function(hello_to_display){
+            // get the hello word
+            const hello_word = hello_to_display.hello;
+            //fill the div with the word
+            hello_id.textContent = hello_word;
+        });
+};
