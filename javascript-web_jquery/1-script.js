6fd19914181f0ce534d763622078085384e6fd62
KevinYeff <kevinyeff94@gmail.com>
2023-09-19 23:13:39 -0500

    fix: smistandard requieres camelCase fixed

--- javascript-web_jquery/1-script.js
+++ javascript-web_jquery/1-script.js
@@ -1,4 +1,4 @@
-//getting the element to update
-const update_text_color = $('header');
+// getting the element to update
+const updateTextColor = $('header');
 // update the text color
-update_text_color.css('color', '#FF0000');
+updateTextColor.css('color', '#FF0000');
